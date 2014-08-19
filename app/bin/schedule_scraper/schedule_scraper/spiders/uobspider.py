488ad2a60de734f1728e3bc206fcf0ac8b8929ab
Yazin S <i@yaz.in>
2014-08-19 16:05:14 +0300

    DB Saving code in; untested

--- app/bin/schedule_scraper/schedule_scraper/spiders/uobspider.py
+++ app/bin/schedule_scraper/schedule_scraper/spiders/uobspider.py
@@ -1,8 +1,11 @@
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 from scrapy.http import Request, FormRequest
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
 import urlparse
 import os, sys, re
+import sqlite3
 
 class UOBSpider(Spider):
   name = "uob"
@@ -10,6 +13,34 @@ class UOBSpider(Spider):
   start_urls = [
     "http://www.online.uob.edu.bh/cgi/enr/schedule2.abrv?prog=1&cyer=2014&csms=1"
   ]
+
+  def __init__(self, *args, **kwargs):
+    # Attach to signals API to process triggers ()
+    dispatcher.connect(self.spider_opened, signal=signals.spider_opened)
+    dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
+    super(UOBSpider, self).__init__(*args, **kwargs)
+
+  def spider_opened(self, spider):
+    # Create our database
+    self.db = sqlite3.connect('python.db')
+    self.cursor = self.db.cursor()
+
+    # Create tables
+    self.cursor.executescript("""
+    DROP TABLE IF EXISTS courses;
+    CREATE TABLE courses (id INTEGER PRIMARY KEY, code TEXT, title TEXT, prerequisites VARCHAR(255), credits INT, exam_date DATE, exam_start INT, exam_end INT, created_at DATETIME, updated_at DATETIME);
+
+    DROP TABLE IF EXISTS sections;
+    CREATE TABLE sections (id INTEGER PRIMARY KEY, number TEXT, instructor TEXT, notes TEXT, course_id INT, created_at DATETIME, updated_at DATETIME);
+
+    DROP TABLE IF EXISTS lectures;
+    CREATE TABLE lectures (id INTEGER PRIMARY KEY, start_time INT, end_time INT, room TEXT, section_id INT, created_at DATETIME, updated_at DATETIME);
+    """)
+
+  def spider_closed(self, spider):
+    # Commit the changes and close
+    self.db.commit()
+    self.db.close()
 
   def parse(self, response):
     # We get a list of course blocks (e.g. ACC, ITCE)
@@ -53,21 +84,25 @@ class UOBSpider(Spider):
     # Extract exam timing for this lecture (from the first section)
     # Note: The exam timing is the same for all sections
     exam_row = response.xpath('//table//td/font/text()').extract()
-    exam_date = exam_row[1]
-    exam_start = exam_row[2]
-    exam_end = exam_row[3]
+    if '00:00' == exam_row[1]:
+      # There's no exam
+      exam_date = exam_start = exam_end = 0
+    else:
+      exam_date = exam_row[1]
+      exam_start = exam_row[2]
+      exam_end = exam_row[3]
 
     # Save the course to the DB
-    course = {
-      'credits': course_credits,
-      'code': response.meta['uob_course_code'],
-      'title': response.meta['uob_course_title'],
-      'exam': {
-        'date': exam_date,
-        'start': exam_start,
-        'end': exam_end,
-      }
-    }
+    self.cursor.execute('INSERT INTO courses(code, title, prerequisites, credits, exam_date, exam_start, exam_end) VALUES(:code, :title, :prerequisites, :credits, :exam_date, :exam_start, :exam_end)', {
+        'code': response.meta['uob_course_code'],
+        'title': response.meta['uob_course_title'],
+        'prerequisites': '', # TODO
+        'credits': course_credits,
+        'exam_date': exam_date,
+        'exam_start': time_to_int(exam_start),
+        'exam_end': time_to_int(exam_end)
+      })
+    course_id = self.cursor.lastrowid
 
     # Section information is temporarily stored in this list
     sections = []
@@ -102,8 +137,8 @@ class UOBSpider(Spider):
         # Store them in our sections list
         sections[section_index]['lectures'].append({
             'day': lecture_cols[col],
-            'start_time': lecture_cols[col+1],
-            'end_time': lecture_cols[col+2],
+            'start_time': time_to_int(lecture_cols[col+1]),
+            'end_time': time_to_int(lecture_cols[col+2]),
             'room': lecture_cols[col+3] 
           })
 
@@ -113,4 +148,34 @@ class UOBSpider(Spider):
       # Increment section
       section_index += 1
 
-    # Save section details to the DB: TODO
+    # Iterate through the sections and save the details to the DB
+    for section in sections:
+      # Save section details
+      self.cursor.execute('INSERT INTO sections(number, instructor, notes, course_id) VALUES(:number, :instructor, :notes, :course_id)', {
+        'number': section['section'],
+        'instructor': section['lecturer'],
+        'notes': '', # TODO
+        'course_id': course_id
+        })
+      section_id = self.cursor.lastrowid
+
+      # Iterate through the lectures and save the details
+      for lecture in section.lectures:
+        # Save lecture details
+        self.cursor.execute('INSERT INTO lectures(days, start_time, end_time, rooms, section_id) VALUES(:days, :start_time, :end_time, :rooms, :section_id)', {
+          'days': lecture['day'],
+          'start_time': lecture['start_time'],
+          'end_time': lecture['end_time'],
+          'room': lecture['room'],
+          'section_id': section_id
+          })
+
+def time_to_int(str_time):
+  # This method converts the passed time (in the form 'HH:MM' to an integer value that can be used instead)
+  if type(str_time) is int:
+    # Wha'? Already an integer .. return it as is
+    return str_time
+  else:
+    # You've come to the right place .. convert it to an integer
+    pieces = str_time.split(':')
+    return int(pieces[0])*60 + int(pieces[1])
