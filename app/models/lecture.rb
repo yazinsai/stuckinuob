17a64ebb1196164eae999499bcc2d3591d30e27d
rock <yazin@whitepayments.com>
2017-03-25 17:16:10 +0000

    Added timeline to sections

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,22 +2,23 @@ class Lecture < ApplicationRecord
   belongs_to :section
 
   def initialize
+  end
+
+  def to_timeline
     # Make timeline for lecture
     @timeline = Timeline.new()
     days.split("").each do |i|
       offset = day_index(i) * 60 * 24 # start of day in min.
       @timeline.add( [offset + start_time, offset + end_time] )
     end
-  end
-
-  def to_timeline
+    
     @timeline
   end
 
   private
 
   def day_index(day)
-    case day
+    case day.upcase
       when "U"
         0
       when "M"
@@ -29,7 +30,7 @@ class Lecture < ApplicationRecord
       when "H"
         4
       else
-        -1
+        raise IndexError, "Call to day_index with invalid day (not U,M,T,W or H)"
     end
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,14 @@
 class Section < ApplicationRecord
   has_many :lectures
   belongs_to :course
+
+  def to_timeline
+    @timeline = Timeline.new()
+
+    lectures.each do |lecture|
+      @timeline.add(lecture.to_timeline)
+    end
+
+    @timeline
+  end
 end
